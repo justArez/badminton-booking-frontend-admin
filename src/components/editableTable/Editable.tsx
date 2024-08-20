5f901b45eda62073ddc12fb270caf9e6a2ff6976
CuongNK <cuongnkce161131@fpt.edu.vn>
2024-08-20 20:55:28 +0700

    merge main

---
+++ src/components/editableTable/Editable.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+export const Editable = () =>{
+    return <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Court's Owner</th>
+                    <th>Court's Account</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Court's Address</th>
+                    <th>Date registered</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Home
+                    </td>
+                    <td>This is the main page</td>
+                    <td><span>Live</span></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+}
